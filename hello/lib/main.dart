d773ae5007581efc765b23d7516c828d32aa33db
Çağlar Dursun <dursuncaglar@gmail.com>
2019-05-22 01:22:46 +0300

    Intro 2 Widget

--- hello/lib/main.dart
+++ hello/lib/main.dart
@@ -1,5 +1,22 @@
-import 'package:flutter/material.dart';
+import "package:flutter/material.dart";
 
 main() {
-  
+
+  runApp(HelloApp());
+
+}
+
+class HelloApp extends StatelessWidget {
+
+  build(context) {
+    //Core route widget
+    //() means const to create new instance
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Ne uzun yapı aq."),
+        ),
+      ),
+    );
+  }
 }
