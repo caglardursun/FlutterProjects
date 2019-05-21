63b1e352d619a653b23daaf262821b347489f94c
Çağlar Dursun <dursuncaglar@gmail.com>
2019-05-22 01:57:33 +0300

    widget structure tested

--- hello/lib/main.dart
+++ hello/lib/main.dart
@@ -1,22 +1,43 @@
 import "package:flutter/material.dart";
+//Notes :
+// I didn't like the whole class structure in first impression
 
-main() {
+// main()
+// {
+//   runApp(HelloApp());
+// }
 
-  runApp(HelloApp());
-
-}
+void main() => runApp(HelloApp());
 
 class HelloApp extends StatelessWidget {
-
-  build(context) {
+  @override
+  Widget build(context) {
     //Core route widget
     //() means const to create new instance
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Ne uzun yapı aq."),
-        ),
+        home: Scaffold(
+      appBar: AppBar(
+        title: Text("What a long nested shit :/"),
       ),
-    );
+      body: Column(
+        children: [
+          Container(
+            margin: EdgeInsets.all(10),
+            child: RaisedButton(
+              onPressed: () {},
+              child: Text("Add Photo"),
+            ),
+          ),
+          Card(
+            child: Column(
+              children: <Widget>[
+                Image.asset("assets/mydaughter.jpg"),
+                Text("Meet Ada 4 the name of Ada Lovelace")
+              ],
+            ),
+          )
+        ],
+      ),
+    ));
   }
 }
